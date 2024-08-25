556f08d3382044d0f83e673077b272497ba14770
Nitesh Khare <niteskak25@gmail.com>
2024-08-25 17:34:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
-function App() {
+const Crypto = createContext();
+
+const CryptoProvider = ({ children }) => {
+  const [currency, setCurrency] = useState('INR');
+  const [symbol, setSymbol] = useState('₹');
+
+  useEffect(() => {
+    if (currency === 'INR') setSymbol('₹');
+    else if (currency === 'USD') setSymbol('$');
+  }, [currency]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Crypto.Provider value={{ currency, setCurrency, symbol }}>
+      {children}
+    </Crypto.Provider>
   );
-}
+};
 
-export default App;
+export default CryptoProvider;
+
+export const useCrypto = () => {
+  return useContext(Crypto);
+};
